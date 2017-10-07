dc093e298822f120a4729f9f27aab97bf93d4855
LaurentPepin <laurent.pepin@polymtl.ca>
2017-10-07 16:15:22 -0400

    cerveau

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,10 +1,15 @@
 import * as express from 'express';
-import { GameInfo, Tile, Point } from '../interfaces';
+import { GameInfo, Tile, Point, UpgradeType } from '../interfaces';
 import { AIHelper } from '../aiHelper';
 
 module Route {
 
     export class Index {
+
+        private static destination: Point;
+
+        private static price = 15000;
+        private static upgradeType = UpgradeType.CarryingCapacity;
 
         private static decompressMap(compressedMap: any): Tile[][] {
             const map = new Array<Array<Tile>>();
@@ -33,49 +38,84 @@ module Route {
             return false;
         }
 
-        private static getAction(map: Tile[][], gameInfo: GameInfo) {
-            let destination: Point;
+        private static destinationIsHome(gameInfo: GameInfo) {
+            return Index.destination.X === gameInfo.Player.HouseLocation.X && Index.destination.Y === gameInfo.Player.HouseLocation.Y;
+        }
 
-            if (Index.backPackIsFull(gameInfo)) {
-                // Call A* on houseLocation, get next point
-                if (gameInfo.Player.Position.X > 25) {
-                    destination = new Point(gameInfo.Player.Position.X - 1, gameInfo.Player.Position.Y);
-                } else if (gameInfo.Player.Position.Y > 27) {
-                    destination = new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y - 1);
-                }
-            } else {
-                if (gameInfo.Player.Position.Y < 35) {
-                    destination = new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y + 1);
-                } else if (gameInfo.Player.Position.X < 27) {
-                    destination = new Point(gameInfo.Player.Position.X + 1, gameInfo.Player.Position.Y);
-                }
+        private static destinationIsNotAMine(mines: Array<Tile>) {
+            return mines.every((mine: Tile) => {
+                return Index.destination.X !== mine.Position.X && Index.destination.Y !== mine.Position.Y;
+            });
+        }
+
+        private static changePrice(): void {
+            switch (Index.price) {
+                case 15000:
+                    Index.price = 50000;
+                    break;
+                case 50000:
+                    Index.price = 100000;
+                    break;
+                case 100000:
+                    Index.price = 250000;
+                    break;
+                case 250000:
+                    Index.price = 500000;
+                    break;
+                case 500000:
+                    Index.price = 15000;
+                    Index.upgradeType = Index.upgradeType + 1;
+                    break;
+                default:
+                    Index.price = 500000;
+            }
+        }
+
+        private static getAction(map: Tile[][], gameInfo: GameInfo) {
+            const home = gameInfo.Player.HouseLocation;
+            const currentPosition = gameInfo.Player.Position;
+            const mines = Index.getMinerals(map);
+            if (Index.destination !== undefined && !Index.destinationIsHome(gameInfo) && Index.destinationIsNotAMine(mines)) {
+                Index.destination = Index.getPathToDestination(home, currentPosition);
+                return AIHelper.createMoveAction(Index.destination);
             }
 
-            // At this moment, getMineableMinerals, get opponentsToKill
-            let mineableMinerals = Index.getMineableMinerals(map, gameInfo);
-
+            const mineableMinerals = Index.getMineableMinerals(map, gameInfo);
             if (mineableMinerals.length > 0 && !Index.backPackIsFull(gameInfo)) {
                 console.log('Mining');
                 console.log(gameInfo.Player.CarriedResources);
                 return AIHelper.createCollectAction(mineableMinerals[0]);
+            } else if (Index.backPackIsFull(gameInfo)) {
+                Index.destination = Index.getPathToDestination(home, currentPosition);
+                return AIHelper.createMoveAction(Index.destination);
             } else {
-                return AIHelper.createMoveAction(destination);
+                if (Index.destination === undefined) {
+                    if (gameInfo.Player.TotalResources >= Index.price) {
+                        const action = AIHelper.createUpgradeAction(Index.upgradeType);
+                        Index.changePrice();
+                        return action;
+                    }
+                    Index.destination = Index.getNearestMine();
+                    return AIHelper.createMoveAction(Index.destination);
+                } else if (Index.destination === home) {
+                    Index.destination = undefined;
+                    return;
+                } else {
+                    Index.destination = Index.getPathToDestination(home, currentPosition);
+                    return AIHelper.createMoveAction(Index.destination);
+                }
             }
         }
 
         private static getMineableMinerals (map: Tile[][], gameInfo: GameInfo): Array<Point> {
-            let mineableMinerals = new Array();
-            let mineralOnSight = Index.getMinerals(map);
+            const mineableMinerals = new Array();
+            const mineralOnSight = Index.getMinerals(map);
             mineralOnSight.forEach((tile: Tile) => {
                 if (tile.Position.Distance(gameInfo.Player.Position) <= 1) {
                     mineableMinerals.push(tile.Position);
                 }
             });
             return mineableMinerals;
-        }
-
-        private static moveTo(position: Point) {
-            return AIHelper.createMoveAction(position);
         }
 
         private static getMinerals(map: Tile[][]): Array<Tile> {
